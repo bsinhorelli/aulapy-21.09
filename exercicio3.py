299cc9b2229f54fa05e752d84b5c0dae4c56d1d0
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-09-21 20:08:08 -0300

    exercicio 2 e 3 resolvidos

---
+++ exercicio3.py
@@ -0,0 +1,16 @@
+# 3. Dadas duas nota, verificar se elas são válidas. Se sim, calcular a média.
+# Caso uma das notas não seja válida, encerrar o programa.
+
+n1 = float(input('Nota 1: '))
+if n1 >= 0 and n1 <= 10:
+    n2 = float(input('Nota 2: '))
+    if n2 >= 0 and n2 <= 10:
+        media = (n1+n2) / 2
+        print(f'Média das duas notas: {media}')
+    else:
+        print('nota 2 inválida')
+
+
+else:
+    print('nota 1 inválida')
+
