f7ba232e89d84363896d9bc131ed143aa60fc220
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-09-21 21:18:43 -0300

    resolucao de exercicios

--- exercicio3.py
+++ exercicio3.py
@@ -6,6 +6,8 @@ if n1 >= 0 and n1 <= 10:
     n2 = float(input('Nota 2: '))
     if n2 >= 0 and n2 <= 10:
         media = (n1+n2) / 2
+        if media > 7:
+            media += 1
         print(f'Média das duas notas: {media}')
     else:
         print('nota 2 inválida')
---
+++ exercicio4.py
@@ -0,0 +1,6 @@
+# 4. Dados 3 valores, verificar e exibir o de maior valor.
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+
+
